a890053b5daed91682a221d64feee0fcb509b41f
unknown <DMegha0504>
2023-06-09 10:15:24 -0700

    First project

---
+++ java/pyramid.java
@@ -0,0 +1,12 @@
+public class pyramid{
+public static void main(String args[]){
+int rows = 5,number=1;
+for(int i=1;i<=rows;i++){
+for(int j=1;j<=i;j++){
+System.out.println(number+ " ");
+number++;
+}
+System.out.println();
+}
+}
+}
